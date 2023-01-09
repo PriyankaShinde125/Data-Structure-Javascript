a74c5266679ef5033c00877f991fa9d298ffaacc
PriyankaShinde125 <pkshinde125@gmail.com>
2023-01-09 21:29:36 +0530

    ADDED : Code to get second max and secon min

--- Array/smallestandlargestfromarray.js
+++ Array/smallestandlargestfromarray.js
@@ -6,6 +6,18 @@ function getMin(arr) {
     return Math.min.apply(null, arr);
 }
 
+function getSecondLargest(arr) {
+    arr.sort(function (a, b) { return b - a });
+    console.log(arr);
+    return arr[1];
+}
+
+function getSecondSmallest(arr) {
+    arr.sort(function (a, b) { return a - b });
+    console.log(arr);
+    return arr[1];
+}
+
 let numberArr = new Array(10);
 
 for (let i = 0; i < numberArr.length; i++) {
@@ -16,5 +28,9 @@ for (let i = 0; i < numberArr.length; i++) {
 console.log(numberArr);
 let largest = getMax(numberArr);
 let smallest = getMin(numberArr);
+let secondLargest = getSecondLargest(numberArr);
+let secondSmallest = getSecondSmallest(numberArr);
 console.log("Largest = " + largest);
 console.log("Smallest = " + smallest);
+console.log("Second largest = " + secondLargest);
+console.log("Second Smallest = " + secondSmallest);
